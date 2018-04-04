2776d3e62dfaca6f7bfd8100446aca592f885d0a
Srujan Routhu <srujan.routhu@gmail.com>
2018-04-04 13:27:22 -0400

    connecting to couch

--- hubs/views.py
+++ hubs/views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from __future__ import unicode_literals
 
 
+import couchdb
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 @api_view(['GET'])
 def charging_pattern(request):
-
-    return Response({'test': 'this is a test'})
+    couch = couchdb.Server('https://admin:f3f28f363581@couchdb-597ada.smileupps.com')
+    db = couch['overrides']
+    for row in db.view('_all_docs', include_docs=True):
+        print(row.id, row.doc)
+    # Call AI process
+    return Response({'charge': True})
